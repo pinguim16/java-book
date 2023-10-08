c0553c84ab046f9941c1861d0c74182bd74c9b91
cesaraugustosi16@gmail.com <cesaraugustosi16@gmail.com>
2023-10-08 13:23:17 -0300

    Capitulo Boolean

---
+++ exemplos/ExemploBooleanoErro.java
@@ -0,0 +1,10 @@
+public class ExemploBooleano {
+
+    public static void main(String[] args) {
+        boolean loginComSucesso = true;
+        boolean loginBloqueado = false;
+
+        System.out.println(loginComSucesso);
+        System.out.println(loginBloqueado);
+    }
+}
